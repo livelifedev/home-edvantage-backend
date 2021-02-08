646de23a13c7e5351ef811cf8d80c8585bc66bc4
John <johnrubio93@gmail.com>
2021-02-08 21:40:07 +1100

    setup auth and admin

--- keystone.ts
+++ keystone.ts
@@ -1,4 +1,10 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
 import 'dotenv/config';
 
 const databaseURL =
@@ -9,19 +15,32 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-  },
-  lists: createSchema({}),
-  ui: {
-    isAccessAllowed: () => true,
-  },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: { fields: ['name', 'email', 'password'] },
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({ User }),
+    ui: {
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), { User: 'id' }),
+  })
+);
